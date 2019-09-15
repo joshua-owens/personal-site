9e0cca3cdaeb77559e1d1488e9b1b8021e91ffda
Joshua Owens <joshuaowens@protonmail.com>
2019-09-14 21:58:19 -0500

    Scaffolding out template

---
+++ data/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	defaultTitle: 'Josh Owens',
+	author: 'Josh Owens',
+	url: 'https://jowens.dev',
+	legalName: 'Josh Owens',
+	defaultDescription: 'I’m Josh and I’m a Fullstack Developer',
+	socialLinks: {
+		twitter: 'http://www.twitter.com/',
+		github: 'https://github.com/joshua-owens',
+		linkedin: 'https://www.linkedin.com/in/joshuasowens',
+	},
+	googleAnalyticsID: 'UA-88875900-4',
+	themeColor: '#6b63ff',
+	backgroundColor: '#6b63ff',
+	address: {
+		city: 'City',
+		region: 'Region',
+		country: 'Country',
+		zipCode: 'ZipCode',
+	},
+	contact: {
+		email: 'email',
+		phone: 'phone number',
+	},
+	foundingDate: '2018',
+	recaptcha_key: '6Lcs6lQUAAAAAEwhNH2IsobIe2csdda4TU3efpMN'
+}
